43ed1eea226a7af2aa4cecfb642d04ed1618ffc5
Ben Cuff <ben.cuff@ukhsa.gov.uk>
2024-05-29 08:23:56 +0100

    Changed expandable script to js

---
+++ docs/_includes/expandable-sections.js
@@ -0,0 +1,22 @@
+const expandables = document.getElementsByClassName("expandable-header");
+  
+const expand = function(){
+  target = this.getAttribute('aria-controls');
+  expanded = this.getAttribute('aria-expanded');
+    
+  if(expanded == "true"){
+    console.log("1");
+    console.log(expanded);
+    document.getElementById(target).setAttribute("aria-hidden", true);
+    this.setAttribute("aria-expanded", false);
+  } else if(expanded == "false"){
+    console.log("2");
+    console.log(expanded);
+    document.getElementById(target).setAttribute("aria-hidden", false);
+    this.setAttribute("aria-expanded", true);
+  }
+}
+  
+for (var i = 0; i < expandables.length; i++) {
+  expandables[i].addEventListener('click', expand);
+}
