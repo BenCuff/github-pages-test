76af738597679b5892759b023537087f19f7bc37
Ben Cuff <ben.cuff@ukhsa.gov.uk>
2024-06-19 12:15:43 +0100

    Removed console comments

--- docs/_includes/expandable-sections.js
+++ docs/_includes/expandable-sections.js
@@ -5,13 +5,9 @@ const expand = function(){
   expanded = this.getAttribute('aria-expanded');
     
   if(expanded == "true"){
-    console.log("1");
-    console.log(expanded);
     document.getElementById(target).setAttribute("aria-hidden", true);
     this.setAttribute("aria-expanded", false);
   } else if(expanded == "false"){
-    console.log("2");
-    console.log(expanded);
     document.getElementById(target).setAttribute("aria-hidden", false);
     this.setAttribute("aria-expanded", true);
   }
